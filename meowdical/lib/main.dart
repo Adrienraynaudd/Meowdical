5ac7c251e21094cd62fb555d182b45569acfd654
ARAYNAUD <adrien.raynaud@ynov.com>
2023-12-11 15:25:17 +0100

    bruh

--- meowdical/lib/main.dart
+++ meowdical/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'Auth/Register.dart' as Register;
+import 'Auth/Login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,24 +14,28 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Notes',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Notes Application with Firebase'),
+      routes: {
+        '/auth': (context) => Register.AuthPage(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
@@ -36,12 +43,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Function to navigate to the authentication page
+  void _navigateToAuthPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Register.AuthPage()),
+    );
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  // Function to navigate to the login page
+  void _navigateToLoginPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
+  }
+
+  // Function to sign out the user
+  void _signOut() async {
+    await FirebaseAuth.instance.signOut();
+    setState(() {});
   }
 
   @override
@@ -49,31 +70,69 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          // Show login icon if the user is not logged in
+          if (FirebaseAuth.instance.currentUser == null)
+            IconButton(
+              icon: Icon(Icons.login),
+              onPressed: _navigateToAuthPage,
+            ),
+          // Show user icon if the user is not logged in
+          if (FirebaseAuth.instance.currentUser == null)
+            IconButton(
+              icon: Icon(Icons.person),
+              onPressed: _navigateToLoginPage,
+            ),
+          // Show logout icon if the user is logged in
+          if (FirebaseAuth.instance.currentUser != null)
+            IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: _signOut,
+            ),
+        ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            // Show a welcome message and user details if logged in
+            if (FirebaseAuth.instance.currentUser != null)
+              Column(
+                children: [
+                  CircleAvatar(
+                    // You can display user's profile picture here
+                    radius: 40,
+                    backgroundColor: Colors.grey[300],
+                    // Placeholder or user's profile picture
+                    child: Icon(Icons.person, size: 60, color: Colors.grey),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Welcome ${FirebaseAuth.instance.currentUser!.displayName ?? ''}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Email: ${FirebaseAuth.instance.currentUser!.email ?? ''}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            if (FirebaseAuth.instance.currentUser == null)
+              Text(
+                'Log in to access notes',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
